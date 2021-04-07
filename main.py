2d5039adb80074300f84558d7d498869e1111683
pastosm <pastosmarios@gmail.com>
2021-04-07 15:28:08 +0100

    pre

--- main.py
+++ main.py
@@ -1,20 +1,78 @@
-import gym
-import vizdoomgym
 import torch
+from torch import nn
+from torchvision import transforms as T
+from PIL import Image
+import numpy as np
+import vizdoomgym
+from pathlib import Path
+from collections import deque
+import random, datetime, os, copy
+
+# Gym is an OpenAI toolkit for RL
+import gym
+from gym.spaces import Box
+from gym.wrappers import FrameStack
+
 env = gym.make('VizdoomCorridor-v0')
+
+
+class GrayScaleObservation(gym.ObservationWrapper):
+
+    def __init__(self, env):
+        super().__init__(env)
+        obs_shape = self.observation_space.shape[:2]
+        self.observation_space = Box(low=0, high=255, shape=obs_shape, dtype=np.uint8)
+
+    def permute_orientation(self, observation):
+        # permute [H, W, C] array to [C, H, W] tensor
+        observation = np.transpose(observation, (2, 0, 1))
+        observation = torch.tensor(observation.copy(), dtype=torch.float)
+        return observation
+
+    def observation(self, observation):
+        observation = self.permute_orientation(observation)
+        transform = T.Grayscale()
+        observation = transform(observation)
+        return observation
+
+class SkipFrame(gym.Wrapper):
+
+    def __init__(self, env, skip):
+        """Return only every `skip`-th frame"""
+        super().__init__(env)
+        self._skip = skip
+
+    def step(self, action):
+        """Repeat action, and sum reward"""
+        total_reward = 0.0
+        done = False
+        for i in range(self._skip):
+            # Accumulate reward and repeat the same action
+            obs, reward, done, info = self.env.step(action)
+            total_reward += reward
+            if done:
+                break
+        return obs, total_reward, done, info
+
+
+
 
 # state.shape = (240, 320, 3)
 
 # use like a normal Gym environment
-state = env.reset()
+
+env = SkipFrame(env, skip=4)
+env = GrayScaleObservation(env)
+env = FrameStack(env, num_stack=4)
+env.reset()
+
 while True:
     state, reward, done, info = env.step(env.action_space.sample())
     # print(state, reward, done, info)
-    # print(state.shape)
+    print(state.shape)
     env.render()
+
     if done:
         env.reset()
-
-print("test")
 
 env.close()
