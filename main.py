3f73b1a0b07064b75f1a3437113f7fdf066024f8
pastosm <pastosmarios@gmail.com>
2021-04-07 16:12:45 +0100

    stuff

--- main.py
+++ main.py
@@ -13,6 +13,31 @@ import vizdoomgym
 env = gym.make('VizdoomBasic-v0')
 
 
+def save_images(state, i):
+    img = Image.fromarray(state)
+    img.save(f'test_images/frame{i}.png')
+
+
+class SkipFrame(gym.Wrapper):
+    def __init__(self, env, skip):
+        """Return only every `skip`-th frame"""
+        super().__init__(env)
+        self._skip = skip
+
+    def step(self, action):
+        """Repeat action, and sum reward"""
+        total_reward = 0.0
+        done = False
+        for i in range(self._skip):
+            # Accumulate reward and repeat the same action
+            obs, reward, done, info = self.env.step(action)
+            total_reward += reward
+            if done:
+                break
+        return obs, total_reward, done, info
+
+
+
 class Grayscale(gym.Wrapper):
     """
     Takes the environment as an input and returns an environment where the states are transformed to
@@ -32,26 +57,33 @@ class Grayscale(gym.Wrapper):
         gray_sc_img = transform(gray_sc_img)
         return gray_sc_img, reward, done, info
 
-    def visualize(self, state):
+    def visualize(self, state, i):
         state = state.numpy()                   # We need to first convert the tensor back to a numpy vector
         state = np.transpose(state, (1, 2, 0))  # We then need to undo our format change
         data = np.squeeze(state,axis=2)         # This is done as a workaround against a pillow bug when dealing with images with one channel
-        image = Image.fromarray(data.astype(np.uint8))
-        image.show()
-        image.close()
-        time.sleep(4)
+        image = Image.fromarray(data.astype(np.uint8), 'L')
+        # image.show()
+        # save_images(data,i)
+        image.save(f'test_images/frame{i}.png')
+        # image.close()
+        # time.sleep(4)
 
 
 
 
+print(env.observation_space)
+env = SkipFrame(env, skip=4)
 env = Grayscale(env)
+print(env.observation_space)
+env = FrameStack(env, num_stack=4)
 
-while True:
+for i in range(5):
     next_state, reward, done, info = env.step(env.action_space.sample())
     env.render()
     # To visualize the grayscale equivalent.
-    Grayscale(env).visualize(next_state)
+    # Grayscale(env).visualize(next_state,i)
 
     if done:
         env.reset()
+
 
