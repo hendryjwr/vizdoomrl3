f6a0dafdd9b9e61edef4e7f9730dc2d50a577a06
MoonClinical <boy.mehdi@live.fr>
2021-04-13 12:29:38 +0100

    Preprocessing done.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import torch
 from torch import nn
-from torchvision import transforms as T
+from torchvision import transforms
 from PIL import Image
 import numpy as np
 import vizdoomgym
@@ -16,95 +16,64 @@ from gym.wrappers import FrameStack
 env = gym.make('VizdoomCorridor-v0')
 
 
-class GrayScaleObservation(gym.ObservationWrapper):
+class ImagePreProcessing(gym.ObservationWrapper):
 
-    def __init__(self, env):
+    def __init__(self, env, shape):
         super().__init__(env)
-        obs_shape = self.observation_space.shape[:2]
-        self.observation_space = Box(low=0, high=255, shape=obs_shape, dtype=np.uint8)
+        self.resize_shape = shape
+        height, width, _ = env.observation_space.shape
+        self.observation_space = Box(0, 255, shape=self.resize_shape, dtype=np.uint8)
 
-    def permute_orientation(self, observation):
-        # permute [H, W, C] array to [C, H, W] tensor
-        observation = np.transpose(observation, (2, 0, 1))
-        observation = torch.tensor(observation.copy(), dtype=torch.float)
+    def observation(self, state):
+        # Applies Grayscale
+        state = np.transpose(state, (2, 0, 1))
+        to_convert = state.copy()
+        converted_state = torch.tensor(to_convert, dtype=torch.float32)
+        transform = transforms.Grayscale()
+        converted_state = transform(converted_state)
+
+        # Applies resizing
+
+        transformation = transforms.Compose([transforms.Resize(self.shape), transforms.Normalize(0, 255)])
+        # Uncomment this is for visualization
+        # transformation = transforms.Resize(self.resize_shape)
+        observation = transformation(converted_state).squeeze(0)
         return observation
 
-    def observation(self, observation):
-        observation = self.permute_orientation(observation)
-        transform = T.Grayscale()
-        observation = transform(observation)
-        return observation
 
-
-def visualize(state, i):
+def visualise(state, i):
     state = state.__array__()
-    # print(state.shape)
-    # print('state before', state)
     state = state[0]
-    # print(state.shape)
-    # print('\n')
-    # print('state after', state)
-
-    # state = np.squeeze(state, axis=0)
-    # state = state.squeeze(0, axis= 3)
-    # state = np.transpose(state, (1, 2, 0))              # We then need to undo our format change
-    # data = np.squeeze(state,
-    #                   axis=2)                           # This is done as a workaround against a pillow bug when dealing with images with one channel
     image = Image.fromarray(state.astype(np.uint8))
-    # print(state)
     image.save(f'test_images/frame{i}.png')
 
 
 class SkipFrame(gym.Wrapper):
 
     def __init__(self, env, skip):
-        """Return only every `skip`-th frame"""
         super().__init__(env)
-        self._skip = skip
+        self.frames_to_skip = skip
 
     def step(self, action):
-        """Repeat action, and sum reward"""
-        total_reward = 0.0
-        done = False
-        for i in range(self._skip):
-            # Accumulate reward and repeat the same action
-            obs, reward, done, info = self.env.step(action)
-            total_reward += reward
+        reward_sum = 0.0
+        for _ in range(self.frames_to_skip):
+            new_state, reward, done, info = self.env.step(action)
+            reward_sum += reward
             if done:
                 break
-        return obs, total_reward, done, info
-
-
-class ResizeObservation(gym.ObservationWrapper):
-    def __init__(self, env, shape):
-        super().__init__(env)
-        self.shape = shape
-
-
-        obs_shape = self.shape + self.observation_space.shape[2:]
-        self.observation_space = Box(low=0, high=255, shape=obs_shape, dtype=np.uint8)
-
-    def observation(self, observation):
-        transforms = T.Compose(
-            [T.Resize(self.shape), T.Normalize(0, 255)]
-        )
-        # Uncomment this is for visualization
-        # transforms = T.Resize(self.shape)
-        observation = transforms(observation).squeeze(0)
-        return observation
+        return new_state, reward_sum, done, info
 
 
 env = SkipFrame(env, skip=4)
-env = GrayScaleObservation(env)
-env = ResizeObservation(env, shape=(120, 160))
+env = ImagePreProcessing(env, shape=(120, 160))
 env = FrameStack(env, num_stack=4)
 env.reset()
 
-for i in range(100):
+for i in range(4):
     state, reward, done, info = env.step(env.action_space.sample())
     env.render()
     # print(state.shape)
-    visualize(state, i)
+    visualise(state, i)
 
     if done:
         env.reset()
