7a7e546356ab5925a6a45a3e7544b794cc9ee195
James Hendry <hendry103@gmail.com>
2021-04-15 17:42:57 +0100

    added metric logging

--- main.py
+++ main.py
@@ -6,14 +6,27 @@ import numpy as np
 import vizdoomgym
 from pathlib import Path
 from collections import deque
-import random, datetime, os, copy
+import random, datetime, os, copy, time
 from torchsummary import summary
 from torchvision import models
 import gym
 from gym.spaces import Box
 from gym.wrappers import FrameStack
+import matplotlib.pyplot as plt
 
 mini_batch_size = 32
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('Using device:', device)
+print()
+
+if device.type == 'cuda':
+    print(torch.cuda.device_count())
+    print(torch.cuda.is_available())
+    print(torch.cuda.get_device_name(0))
+    print('Memory Usage:')
+    print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
+    print('Cached:   ', round(torch.cuda.memory_reserved(0)/1024**3,1), 'GB')
 
 env = gym.make('VizdoomCorridor-v0')
 
@@ -192,9 +205,9 @@ class DoomAgent:
 
     def learn(self):
         if self.curr_step % self.learn_every != 0:
-            return
+            return None, None
         if self.curr_step < 10000:
-            return
+            return None, None
 
         # Step 1: Recall from memory
         current_state_array, new_state_array, action_array, reward_array, done_array = experience.recall()
@@ -206,12 +219,14 @@ class DoomAgent:
         target_q_values = self.td_target(new_state_array, reward_array, done_array)        
 
         # Step 4: Perform gradient descent
-        self.update(current_q_values, target_q_values)
+        loss_value = self.update(current_q_values, target_q_values)
 
         # Step 5: Sync online network with target network
 
         if self.curr_step % self.syncing_frequency == 0:
             self.neural_net.target.load_state_dict(self.neural_net.online.state_dict())
+
+        return (current_q_values.mean().item(), loss_value)
 
     def td_estimate(self, current_state_array, action_array):
         indexing_array = np.arange(0, mini_batch_size)
@@ -235,8 +250,7 @@ class DoomAgent:
         loss.backward()
         self.optimizer.step()
         self.optimizer.zero_grad()
-        if self.curr_step % 50 == 0:
-            print(loss.item())
+        return loss.item()
 
 
 class ExperienceReplay:
@@ -269,30 +283,138 @@ class ExperienceReplay:
         return state, next_state, action.squeeze(), reward.squeeze(), done.squeeze()
 
 
+# The following class is taken from https://github.com/yuansongFeng/MadMario/ for metric logging of data. 
+class MetricLogger:
+    def __init__(self, save_dir):
+        self.save_log = save_dir / "log"
+        with open(self.save_log, "w") as f:
+            f.write(
+                f"{'Episode':>8}{'Step':>8}{'Epsilon':>10}{'MeanReward':>15}"
+                f"{'MeanLength':>15}{'MeanLoss':>15}{'MeanQValue':>15}"
+                f"{'TimeDelta':>15}{'Time':>20}\n"
+            )
+        self.ep_rewards_plot = save_dir / "reward_plot.jpg"
+        self.ep_lengths_plot = save_dir / "length_plot.jpg"
+        self.ep_avg_losses_plot = save_dir / "loss_plot.jpg"
+        self.ep_avg_qs_plot = save_dir / "q_plot.jpg"
+
+        # History metrics
+        self.ep_rewards = []
+        self.ep_lengths = []
+        self.ep_avg_losses = []
+        self.ep_avg_qs = []
+
+        # Moving averages, added for every call to record()
+        self.moving_avg_ep_rewards = []
+        self.moving_avg_ep_lengths = []
+        self.moving_avg_ep_avg_losses = []
+        self.moving_avg_ep_avg_qs = []
+
+        # Current episode metric
+        self.init_episode()
+
+        # Timing
+        self.record_time = time.time()
+
+    def log_step(self, reward, loss, q):
+        self.curr_ep_reward += reward
+        self.curr_ep_length += 1
+        if loss:
+            self.curr_ep_loss += loss
+            self.curr_ep_q += q
+            self.curr_ep_loss_length += 1
+
+    def log_episode(self):
+        "Mark end of episode"
+        self.ep_rewards.append(self.curr_ep_reward)
+        self.ep_lengths.append(self.curr_ep_length)
+        if self.curr_ep_loss_length == 0:
+            ep_avg_loss = 0
+            ep_avg_q = 0
+        else:
+            ep_avg_loss = np.round(self.curr_ep_loss / self.curr_ep_loss_length, 5)
+            ep_avg_q = np.round(self.curr_ep_q / self.curr_ep_loss_length, 5)
+        self.ep_avg_losses.append(ep_avg_loss)
+        self.ep_avg_qs.append(ep_avg_q)
+
+        self.init_episode()
+
+    def init_episode(self):
+        self.curr_ep_reward = 0.0
+        self.curr_ep_length = 0
+        self.curr_ep_loss = 0.0
+        self.curr_ep_q = 0.0
+        self.curr_ep_loss_length = 0
+
+    def record(self, episode, epsilon, step):
+        mean_ep_reward = np.round(np.mean(self.ep_rewards[-100:]), 3)
+        mean_ep_length = np.round(np.mean(self.ep_lengths[-100:]), 3)
+        mean_ep_loss = np.round(np.mean(self.ep_avg_losses[-100:]), 3)
+        mean_ep_q = np.round(np.mean(self.ep_avg_qs[-100:]), 3)
+        self.moving_avg_ep_rewards.append(mean_ep_reward)
+        self.moving_avg_ep_lengths.append(mean_ep_length)
+        self.moving_avg_ep_avg_losses.append(mean_ep_loss)
+        self.moving_avg_ep_avg_qs.append(mean_ep_q)
+
+        last_record_time = self.record_time
+        self.record_time = time.time()
+        time_since_last_record = np.round(self.record_time - last_record_time, 3)
+
+        print(
+            f"Episode {episode} - "
+            f"Step {step} - "
+            f"Epsilon {epsilon} - "
+            f"Mean Reward {mean_ep_reward} - "
+            f"Mean Length {mean_ep_length} - "
+            f"Mean Loss {mean_ep_loss} - "
+            f"Mean Q Value {mean_ep_q} - "
+            f"Time Delta {time_since_last_record} - "
+            f"Time {datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')}"
+        )
+
+        with open(self.save_log, "a") as f:
+            f.write(
+                f"{episode:8d}{step:8d}{epsilon:10.3f}"
+                f"{mean_ep_reward:15.3f}{mean_ep_length:15.3f}{mean_ep_loss:15.3f}{mean_ep_q:15.3f}"
+                f"{time_since_last_record:15.3f}"
+                f"{datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S'):>20}\n"
+            )
+
+        for metric in ["ep_rewards", "ep_lengths", "ep_avg_losses", "ep_avg_qs"]:
+            plt.plot(getattr(self, f"moving_avg_{metric}"))
+            plt.savefig(getattr(self, f"{metric}_plot"))
+            plt.clf()
+
+
 # summary(DoomNN((4, 120, 160), env.action_space.n), (4, 120, 160))
 
 # 2. Making the epsilon greedy policy
 # 3. Implementing the Q learning pseudocode
 
+
+save_dir = Path("checkpoints") / datetime.datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
+save_dir.mkdir(parents=True)
+
 ddqn_agent = DoomAgent(env.observation_space.shape, env.action_space.n)
+logger= MetricLogger(save_dir)
 experience = ExperienceReplay()
 
 
+
 def play():
-    episodes = 1000000000000000000000000000000000
+    episodes = 50000
     for i in range(episodes):
 
         state = env.reset()
         done = False
 
         while not done:
-            if ddqn_agent.curr_step % 1000 == 0:
-                print(ddqn_agent.curr_step)
 
             action = ddqn_agent.act(state)
             new_state, reward, done, info = env.step(action)
             experience.cache(state, new_state, action, reward, done)
-            ddqn_agent.learn()
+            q, loss = ddqn_agent.learn()
+            logger.log_step(reward, loss, q)
             state = new_state
 
             # Step 1 Calculate td_estimate
@@ -306,6 +428,11 @@ def play():
 
             if done:
                 break
+
+        logger.log_episode()
+
+        if i % 20 == 0:
+            logger.record(episode=i, epsilon=ddqn_agent.current_epsilon, step=ddqn_agent.curr_step)
 
 
 play()
